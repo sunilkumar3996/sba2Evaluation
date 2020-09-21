e2d66e85f3f4a11a4b1e6509d289f3518650459f
NavinIIHT <70938679+NavinIIHT@users.noreply.github.com>
2020-09-21 11:28:24 +0530

    Add files via upload

--- src/main/java/com/eval/coronakit/config/SecurityConfig.java
+++ src/main/java/com/eval/coronakit/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.userdetails.User.UserBuilder;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 
+	@SuppressWarnings("deprecation")
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
--- src/main/java/com/eval/coronakit/controller/LoginController.java
+++ src/main/java/com/eval/coronakit/controller/LoginController.java
@@ -1,16 +1,7 @@
 package com.eval.coronakit.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.eval.coronakit.model.UserDetails;
-
-
-
 
 @Controller
 public class LoginController {
