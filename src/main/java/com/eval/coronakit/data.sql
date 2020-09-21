a0cd7e176f3c05b08662bfc67821de322d544433
NavinIIHT <70938679+NavinIIHT@users.noreply.github.com>
2020-09-21 13:26:51 +0530

    Delete data.sql

--- src/main/java/com/eval/coronakit/data.sql
+++
@@ -1,2 +0,0 @@
-insert into users (username, password, enabled, email,contact) values ('admin','{noop}admin',1,'admin@mail.com', '1234567890');
-insert into authorities (username, authority) values ('admin','ROLE_ADMIN');
